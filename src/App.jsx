143c613cc3c571c226e3944aae937fda61ea91e0
Leonardo Pacini <leopacskate@gmail.com>
2025-03-09 22:55:35 -0300

    [Update 3] Media select feature added

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,13 @@
-import { useState } from "react";
 import Canvas from "./components/Canvas.jsx";
 import Screen from "./components/Screen.jsx";
 
 function App() {
-  let [images, setImages] = useState([]);
-
-  const onDragOver = (event) => {
-    event.preventDefault();
-  };
-
-  const onDrop = (event) => {
-    event.preventDefault();
-
-    const file = event.dataTransfer.files[0];
-    if (file && file.type.startsWith("image/")) {
-      const imageReader = new FileReader();
-      imageReader.onload = () => {
-        const image = new Image();
-        image.onload = () => {
-          const newImage = {
-            id: images.length + 1,
-            url: imageReader.result,
-            name: file.name,
-            dimensions: {
-              width: image.width / 4,
-              height: image.height / 4,
-            },
-          };
-
-          setImages([...images, newImage]);
-        };
-        image.src = imageReader.result;
-      };
-      imageReader.readAsDataURL(file);
-    } else {
-      alert("Por favor, insira uma imagem válida.");
-    }
-  };
-
   return (
-    <div className="w-screen h-screen bg-zinc-200 flex justify-center items-center relative">
-      <Canvas images={images} onDragOver={onDragOver} onDrop={onDrop} />
+    <div
+      id="container"
+      className=" w-screen h-screen bg-zinc-200 flex justify-center items-center relative"
+    >
+      <Canvas />
       <Screen />
     </div>
   );
--- src/components/Canvas.jsx
+++ src/components/Canvas.jsx
@@ -1,19 +1,91 @@
-function Canvas(props) {
-  console.log(props.images);
+import { useState } from "react";
+
+function Canvas() {
+  let [images, setImages] = useState([]);
+
+  const onCanvasClick = (event) => {
+    if (event.target.tagName == "IMG") return isMediaSelected(event);
+    if (event.target.tagName != "IMG") {
+      setImages(
+        images.map((img) => {
+          return { ...img, isSelected: false };
+        })
+      );
+    }
+  };
+
+  const onCanvasDragOver = (event) => {
+    event.preventDefault();
+  };
+
+  const onCanvasDrop = (event) => {
+    event.preventDefault();
+    const file = event.dataTransfer.files[0];
+
+    if (file && file.type.startsWith("image/")) {
+      const imageReader = new FileReader();
+      imageReader.onload = () => {
+        const image = new Image();
+        image.onload = () => {
+          const newImage = {
+            id: images.length,
+            url: imageReader.result,
+            name: file.name,
+            isSelected: false,
+            dimensions: {
+              width: image.width / 4,
+              height: image.height / 4,
+            },
+          };
+          setImages([...images, newImage]);
+        };
+        image.src = imageReader.result;
+      };
+      imageReader.readAsDataURL(file);
+    } else {
+      alert("Por favor, insira uma imagem válida.");
+    }
+  };
+
+  const isMediaSelected = (event) => {
+    const SelectedMedia = event.target;
+    const selectedMediaId = parseInt(SelectedMedia.id);
+
+    setImages(() => {
+      const updateSelectedImage = images.map((img) => {
+        if (selectedMediaId != img.id && img.isSelected == true)
+          return { ...img, isSelected: false };
+        if (selectedMediaId == img.id) return { ...img, isSelected: true };
+        return img;
+      });
+      return updateSelectedImage;
+    });
+  };
+
+  const getMediaStyle = (media) => {
+    return media.isSelected
+      ? { outline: "solid 2.5px cadetblue" }
+      : { outline: "none" };
+  };
+
   return (
     <div
+      id="canvas"
       className="w-full h-full absolute z-50"
-      onDragOver={props.onDragOver}
-      onDrop={props.onDrop}
+      onClick={onCanvasClick}
+      onDragOver={onCanvasDragOver}
+      onDrop={onCanvasDrop}
     >
-      {props.images.map((img) => (
+      {images.map((img) => (
         <img
           key={img.id}
+          id={img.id}
           src={img.url}
           alt={img.name}
           width={img.dimensions.width}
           height={img.dimensions.height}
-          className="absolute"
+          style={getMediaStyle(img)}
+          className="image absolute"
         />
       ))}
     </div>
